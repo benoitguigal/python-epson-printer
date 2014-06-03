da7f68e977e2a6ea895f80a85322bc5aea6abba6
benoit <benoit.guigal@gmail.com>
2014-06-03 17:03:21 +0200

    print bitmap

--- Epson_Thermal.py
+++ Epson_Thermal.py
@@ -48,18 +48,84 @@ class Epson_Thermal(object):
         self._write(msg)
 
     def cut(self):
-        bytes = [
+        byte_array = [
             chr(29), # GS
             chr(86), # V
             chr(0)]  # \0
-        command = ''.join(bytes)
+        command = ''.join(byte_array)
         self._write(command)
 
-    # def print_bitmap(self, pixels, w, h):
+    def print_bitmap(self, pixels, w, h):
+
+        # Set line spacing to 24 dots
+        byte_array = [
+            chr(27),    # ESC
+            chr(51),    # 3
+            chr(24)]
+        self._write(''.join(byte_array))
+
+
+        # Calculate nL and nH
+        nh = chr(w / 256)
+        nl = chr(w % 256)
+
+        offset = 0
+
+        while offset < h:
+            byte_array = [
+                chr(27),  # ESC
+                chr(42),  # *
+                chr(33),  # double density mode
+                nl,
+                nh]
+
+            for x in range(w):
+                for k in range(3):
+                    slice = 0
+                    for b in range(8):
+                        y = (((offset / 8) + k) * 8) + b
+                        i = (y * w) + x
+                        v = 0
+                        if i < len(pixels):
+                            v = pixels[i]
+                        slice |= (v << (7 - b))
+
+                    byte_array.append(chr(slice))
+
+            offset += 24
+            self._write(''.join(byte_array))
+            self.linefeed()
+
+
+        # Restore the line spacing to the default 30 dots
+        byte_array = [
+            chr(27),    # ESC
+            chr(51),    # 3
+            chr(30)]
+        self._write(''.join(byte_array))
+
+    def get_pixels_and_dimensions(self, image_path):
+        import Image
+        i = Image.open(image_path)
+        monochrome = i.convert('1')
+        data = monochrome.getdata()
+        pixels = []
+        for i in range(len(data)):
+            if data[i] == 255:
+                pixels.append(0)
+            else:
+                pixels.append(1)
+
+        (w, h) = monochrome.size
+        return (pixels, w, h)
+
 
 
 if __name__ == '__main__':
     printer = Epson_Thermal(0x04b8,0x0e03)  # EPSON TM-T20
     printer.print_text("Hello world")
+    printer.linefeed()
+    (pixels, w, h) = printer.get_pixels_and_dimensions("logo.png")
+    printer.print_bitmap(pixels, w, h)
     printer.linefeed()
     printer.cut()
