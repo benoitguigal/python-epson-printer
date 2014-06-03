3fe353977d9a3df04d78fc7d33c591ed7a6e1232
benoit <benoit.guigal@gmail.com>
2014-06-03 16:38:54 +0200

    USB init, linefeed, print_text and cut

---
+++ Epson_Thermal.py
@@ -0,0 +1,60 @@
+__author__ = 'benoit'
+
+import usb.core
+
+class Epson_Thermal(object):
+
+    printer = None
+
+    def __init__(self, idVendor, idProduct, interface=0, in_ep=0x82, out_ep=0x01):
+        """
+        @param idVendor  : Vendor ID
+        @param idProduct : Product ID
+        @param interface : USB device interface
+        @param in_ep     : Input end point
+        @param out_ep    : Output end point
+        """
+        self.idVendor  = idVendor
+        self.idProduct = idProduct
+        self.interface = interface
+        self.in_ep     = in_ep
+        self.out_ep    = out_ep
+
+        # Search device on USB tree and set is as escpos device
+        self.printer = usb.core.find(idVendor=self.idVendor, idProduct=self.idProduct)
+        if self.printer is None:
+            print "Cable isn't plugged in"
+
+        if self.printer.is_kernel_driver_active(0):
+            try:
+                self.printer.detach_kernel_driver(0)
+            except usb.core.USBError as e:
+                print "Could not detatch kernel driver: %s" % str(e)
+
+        try:
+            self.printer.set_configuration()
+            self.printer.reset()
+        except usb.core.USBError as e:
+            print "Could not set configuration: %s" % str(e)
+
+    def _write(self, msg):
+        self.device.write(self.out_ep, msg, self.interface)
+
+
+    def linefeed(self):
+        self._write(chr(10)) # LF
+
+    def print_text(self, msg):
+        self._write(msg)
+
+    def cut(self):
+        bytes = [
+            chr(29), # GS
+            chr(86), # V
+            chr(0)]  # \0
+        command = ''.join(bytes)
+        self._write(command)
+
+    # def print_bitmap(self, pixels, w, h):
+
+
