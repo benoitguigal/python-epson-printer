c1d23a2850336db707395a60699f601d61954ac5
benoit <benoit.guigal@gmail.com>
2014-06-10 16:48:22 +0200

    resize image before sending it to the printer

--- epson_printer/util.py
+++ epson_printer/util.py
@@ -11,13 +11,17 @@ class BitmapData:
     @classmethod
     def fromFileImage(cls, image_path):
         i = Image.open(image_path)
-        monochrome = i.convert('1')
-        data = monochrome.getdata()
+        (w, h) = i.size
+        if w > 512:
+            ratio = int(w / 512)
+            h = int(h / ratio)
+            i = i.resize((512, h), Image.ANTIALIAS)
+        i = i.convert('1')
+        data = i.getdata()
         pixels = []
         for i in range(len(data)):
             if data[i] == 255:
                 pixels.append(0)
             else:
                 pixels.append(1)
-        (w, h) = monochrome.size
         return cls(pixels, w, h)
