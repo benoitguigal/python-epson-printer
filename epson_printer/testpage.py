df17ee6f2cfc61f6e966eb395708f8d4966d3ed7
roderick <mail@roderick.de>
2014-11-02 16:48:24 +0100

    bugfix in test page arg parsing

--- epson_printer/testpage.py
+++ epson_printer/testpage.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     parser.add_option("-v", "--idvendor", action="store", type="int", dest="id_vendor", help="The printer vendor id")
     parser.add_option("-p", "--idProduct", action="store", type="int", dest="id_product", help="The printer product id")
     options, args = parser.parse_args()
-    if not options.id_vendor or not options.id_vendor:
+    if not options.id_vendor or not options.id_product:
         parser.print_help()
     else:
         printer = EpsonPrinter(options.id_vendor, options.id_product)
