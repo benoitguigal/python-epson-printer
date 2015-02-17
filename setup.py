255d63db48d2918a0d5a3682e7052d6449854d10
benoit <benoit.guigal@gmail.com>
2015-02-17 10:34:42 +0100

    bump to version 1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name="python_epson_printer",
-      version="1.5",
+      version="1.6",
       description="A library to control Epson thermal printers based on ESC/POS language",
       url="https://github.com/benoitguigal/python-epson-printer",
       author="benoitguigal",
